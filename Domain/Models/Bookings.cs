f8d7a37e214d39541dc4d0c58325ef40195e60fb
sergi.lomtadze@gmail.com <sergi.lomtadze@gmail.com>
2022-02-04 00:42:27 +0400

    added models:Countries,Bookings, ApplicationDbContect

---
+++ Domain/Models/Bookings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public  class Bookings:BaseEntity
+    {
+        public int CorrelationId { get; set; }
+
+        public string? BookingType { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
