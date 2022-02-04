6386bfc22578bf22f3b22a2f8281c501466ad192
nroga18 <nroga18@freeuni.edu.ge>
2022-02-04 13:08:06 +0400

    add models: city, floor, office

---
+++ Domain/Models/Floor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Floor : BaseEntity
+    {
+        public int NumPlaces { get; set; }
+        public int FloorNum { get; set; }
+        public bool IsKitchenPresent { get; set; }
+        public bool IsMeetingRoomPresent { get; set; }
+    }
+}
---
+++ Domain/Models/Office.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Office : BaseEntity
+    {
+        public string OfficeName { get; set; }
+        public int NumFloors { get; set; }
+        public string Address { get; set; }
+        public bool IsFreeParkingAvailable { get; set; }
+    }
+}
