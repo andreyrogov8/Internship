f309928893bfae62f0a7cdcddeb630b974cfbeb1
sergi.lomtadze@gmail.com <sergi.lomtadze@gmail.com>
2022-02-07 16:35:27 +0400

    added CountriesProfile

---
+++ Application/Profiles/CountriesProfile.cs
@@ -0,0 +1,21 @@
+﻿using Application.Features.CountryCQ;
+using AutoMapper;
+using MediaBrowser.Model.Globalization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Profiles
+{
+    public class CountriesProfile : Profile
+    {
+        public CountriesProfile()
+        {
+            CreateMap<CountryList, Responce>();
+        }
+    
+        
+    }
+}
