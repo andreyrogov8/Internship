9ccb523b46dbb2931f575be467dfd12a7e0f2c25
sergi.lomtadze@gmail.com <sergi.lomtadze@gmail.com>
2022-02-10 23:27:43 +0400

    remove unused usings from Application DI

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -1,15 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Application.Profiles;
 using MediatR;
 using System.Reflection;
-using Application.Features.CountryCQ;
 using Application.Infrastructure;
 
 namespace Application
