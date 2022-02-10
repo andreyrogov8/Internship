ae9b6e2059af473e3004f3ea090616194bb07e11
nroga18 <nroga18@freeuni.edu.ge>
2022-02-10 19:38:26 +0400

    update base controller

--- WebApi/Controllers/OfficesController.cs
+++ WebApi/Controllers/OfficesController.cs
@@ -7,25 +7,22 @@ using Application.Features.OfficeFeature.Queries;
 
 namespace WebApi.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class OfficesController : BaseController
-    {
-        public OfficesController(IMediator mediator) : base(mediator)
-        {
-        }
+  
+    [ Route("api/[controller]")]
+    public class OfficesController : BaseApiController
+    {        
 
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            var result = await _mediator.Send(new GetOfficeListQueryRequest());
+            var result = await Mediator.Send(new GetOfficeListQueryRequest());
             return Ok(result);
         }
 
         [HttpGet("{id:int}")]
         public async Task<IActionResult> GetById(int id)
         {
-            var result = await _mediator.Send(new GetOfficeByIdQueryRequest { Id = id });
+            var result = await Mediator.Send(new GetOfficeByIdQueryRequest { Id = id });
             return Ok(result);
         }
 
