b99dcbb2d8f7e9d0dd83877b6b00a5d3556246ea
sergi.lomtadze@gmail.com <sergi.lomtadze@gmail.com>
2022-02-14 16:13:22 +0400

    adding Bot in Application

---
+++ Application/TelegramBot.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Telegram.Bot;
+
+namespace Application
+{
+    public class TelegramBot 
+    {
+        private readonly IConfiguration _configuration;
+        private TelegramBotClient _botClient;
+        public TelegramBot(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task<TelegramBotClient> GetBot()
+        {
+            _botClient = new TelegramBotClient(_configuration["Token"]);
+
+            //var hook = $"{_configuration["Url"]}api/message/update";
+            //await _botClient.SetWebhookAsync(hook);
+
+            return _botClient;
+        }
+    }
+}
---
+++ WebApi/Controllers/BotBasicController.cs
@@ -0,0 +1,39 @@
+﻿using Application;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace WebApi.Controllers
+{
+    [Route("api/bot")]
+    [ApiController]
+    public class BotBasicController : ControllerBase
+    {
+        private readonly TelegramBotClient _telegraBotClient;
+        public BotBasicController(TelegramBot telegramBot)
+        {
+            _telegraBotClient = telegramBot.GetBot().Result;
+        }
+        [HttpPost("update")]
+        public async Task<IActionResult> Update([FromBody] object update)
+        {
+            // /start => register user
+
+            var upd = JsonConvert.DeserializeObject<Update>(update.ToString());
+            var chat = upd.Message?.Chat;
+
+            if (chat == null) return BadRequest();
+
+
+            await _telegraBotClient.SendTextMessageAsync(chat.Id, "Hello from bot");
+
+            return Ok();
+        }
+    }
+
+
+
+
+}
