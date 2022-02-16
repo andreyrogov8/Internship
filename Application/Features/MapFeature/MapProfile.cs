2bcee5983e333796314c4f1ffea8f36aef38532c
Asadbek <asindarov07@gmail.com>
2022-02-16 15:45:27 +0500

    Added GetAll, GetById Map queries

---
+++ Application/Features/MapFeature/MapProfile.cs
@@ -0,0 +1,20 @@
+﻿using Application.Features.MapFeature.Queries;
+using AutoMapper;
+using Domain.Models;
+
+namespace Application.Features.MapFeature
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Map, MapDTO>()
+                .ForMember(dest => dest.Country, opt => opt.MapFrom(src => src.Office.Country))
+                .ForMember(dest => dest.City, opt => opt.MapFrom(src => src.Office.City));
+            CreateMap<Map, GetMapByIdQeryResponse>()
+                .ForMember(dest => dest.Country, opt => opt.MapFrom(src => src.Office.Country))
+                .ForMember(dest => dest.City, opt => opt.MapFrom(src => src.Office.City)); 
+
+        }
+    }
+}
---
+++ WebApi/Controllers/MapsController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Features.MapFeature.Queries;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MapsController : BaseApiController
+    {
+        [HttpGet]
+        public async Task<ActionResult<GetMapListQueryRequest>> GetAllAsync()
+        {
+            var result = await Mediator.Send(new GetMapListQueryRequest());
+            return Ok(result);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<GetMapByIdQeryResponse>> GetByIdAsync(int id)
+        {
+            var result = await Mediator.Send(new GetMapByIdQueryRequest { Id = id });
+            return Ok(result);  
+        }
+    }
+}
