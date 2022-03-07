bb9dfa21b055354d0f283ca092ad34355ea58dce
Asadbek <asindarov07@gmail.com>
2022-03-07 16:47:14 +0500

    Add delete vacation functionality

--- Application/Telegram/Keyboards/SendVacationListKeyboard.cs
+++ Application/Telegram/Keyboards/SendVacationListKeyboard.cs
@@ -5,9 +5,24 @@ namespace Application.Telegram.Keyboards
 {
     public static class SendVacationListKeyboard
     {
+        public static InlineKeyboardMarkup SendBackButton()
+        {
+            List<InlineKeyboardButton> buttons = new List<InlineKeyboardButton>();
+            buttons.Add(new InlineKeyboardButton("BACK TO HOME") { CallbackData = "BACKStartingProcess" });
+            var inlineKeyboard = KeyboardHelper.BuildInLineKeyboard(buttons, 1);
+            return inlineKeyboard;
+        }
+
+
         public static InlineKeyboardMarkup BuildKeyboard(IEnumerable<VacationDTO> vacations)
         {
+            int counter = 0;
             List<InlineKeyboardButton> buttons = new();
+            foreach(var vacation in vacations)
+            {
+                counter++;
+                buttons.Add(new InlineKeyboardButton($"{counter}") { CallbackData = $"{vacation.Id}" });
+            }
             buttons.Add(new InlineKeyboardButton("BACK") { CallbackData = "BACKStartingProcess" });
 
             var inlineKeyboard = KeyboardHelper.BuildInLineKeyboard(buttons, 2);
