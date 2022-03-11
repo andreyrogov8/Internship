486b8aae38e2a6dc196cfb1e459bd8b5c570af91
Navruz Rakhimov <rakhimovnavruz@gmail.com>
2022-03-11 22:51:05 +0500

    added unit test for workplace creation and validation

--- Tests/Application.UnitTests/Features/Workplace/CommandTests/CreateWorkplaceCommandTest.cs
+++ Tests/Application.UnitTests/Features/Workplace/CommandTests/CreateWorkplaceCommandTest.cs
@@ -1,9 +1,15 @@
-﻿using Application.Interfaces;
+﻿using Application.Features.WorkplaceFeature.Commands;
+using Application.Interfaces;
 using AutoMapper;
+using Domain.Enums;
+using FluentAssertions;
+using FluentValidation;
+using FluentValidation.TestHelper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -13,18 +19,64 @@ namespace Application.UnitTests.Features.Workplace.CommandTests
     {
         private readonly IApplicationDbContext _context;
         private readonly IMapper _automapper;
+        private readonly Validator _validator;
         public CreateWorkplaceCommandTest(BaseTestFixture baseFixture)
         {
             _context = baseFixture.Context;
             _automapper = baseFixture.Mapper;
+            _validator = new Validator();
         }
 
-        [Fact]
-        public async Task CreateWorkplace()
+        public static IEnumerable<object[]> ValidData
         {
-            // Arrange
-            // Act
-            // Assert
+            get
+            {
+                yield return new object[] { WorkplaceType.ShortTerm, 100, 75 };
+                yield return new object[] { WorkplaceType.LongTerm, 75, 100 };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(ValidData))]
+        public async Task ShouldCreateWorkplace(WorkplaceType workplaceType, int workplaceNumber, int mapId)
+        {
+            var createCommand = new CreateWorkplaceCommandRequest
+            {
+                WorkplaceNumber = workplaceNumber,
+                WorkplaceType = workplaceType,
+                MapId = mapId
+            };
+
+            var handler = new CreateWorkplaceCommandHandler(_context, _automapper);
+
+            var response = await handler.Handle(createCommand, new CancellationToken());
+            response.Id.Should().BeOfType(typeof(int));
+            _context.Workplaces.Count().Should().BeGreaterThanOrEqualTo(1);
+        }
+
+        public static IEnumerable<object[]> InvalidData
+        {
+            get
+            {
+                yield return new object[] { null, 100, 75 };
+                yield return new object[] { WorkplaceType.ShortTerm, 0, 100 };
+                yield return new object[] { WorkplaceType.LongTerm, 100, 0 };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidData))]
+        public async void ShouldRequireMinimumFields(WorkplaceType workplaceType, int workplaceNumber, int mapId)
+        {
+            var createCommand = new CreateWorkplaceCommandRequest
+            {
+                WorkplaceNumber = workplaceNumber,
+                WorkplaceType = workplaceType,
+                MapId = mapId
+            };
+
+            var result = _validator.TestValidate(createCommand);
+            result.ShouldHaveAnyValidationError();
         }
     }
 }
