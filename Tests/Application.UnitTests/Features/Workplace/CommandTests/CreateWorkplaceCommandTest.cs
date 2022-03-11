e73928c78faa0a3835bc7b6a0385f95ac077b666
Navruz Rakhimov <rakhimovnavruz@gmail.com>
2022-03-11 15:08:22 +0500

    unit test setup

--- Tests/Application.UnitTests/Features/Workplace/CommandTests/CreateWorkplaceCommandTest.cs
+++ Tests/Application.UnitTests/Features/Workplace/CommandTests/CreateWorkplaceCommandTest.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using Application.Interfaces;
+using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Application.UnitTests.Features.Workplace.CommandTests
 {
-    public class CreateWorkplaceCommandTest
+    public class CreateWorkplaceCommandTest : IClassFixture<BaseTestFixture>
     {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _automapper;
+        public CreateWorkplaceCommandTest(BaseTestFixture baseFixture)
+        {
+            _context = baseFixture.Context;
+            _automapper = baseFixture.Mapper;
+        }
+
+        [Fact]
+        public async Task CreateWorkplace()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
     }
 }
---
+++ Tests/Application.UnitTests/Infrastructure/AutoMapperFactory.cs
@@ -0,0 +1,22 @@
+﻿using Application.Features.MapFeature;
+using Application.Features.VacationFeature;
+using Application.Profiles;
+using AutoMapper;
+
+namespace Application.UnitTests.Infrastructure
+{
+    public static class AutoMapperFactory
+    {
+        public static IMapper Create()
+        {
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddMaps(typeof(MapProfile),
+                    typeof(WorkplacesProfile),
+                    typeof(VacationProfile));
+            });
+
+            return mappingConfig.CreateMapper();
+        }
+    }
+}
